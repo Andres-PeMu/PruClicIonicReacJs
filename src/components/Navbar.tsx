f5ce5d0637ae7751251db8eb0f3b248b6c6fbf65
Andres-PeMu <andres.pena.m15@gmail.com>
2022-03-24 19:31:23 -0500

    obteniendo Api swapi

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+    return(
+        <nav className="navbar navbar-dark bg-dark">
+            <div className='container'>
+                <a className="navbar-brand" href="/">The Star Wars API</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
